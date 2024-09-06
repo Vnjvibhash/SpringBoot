0f22b09ff32b8e7e4a44f4241bf049f3ec519b5a
Vivek Kumar <vnjvibhash@gmail.com>
2024-09-06 17:19:54 +0530

    First App

---
+++ firstapp/src/main/java/in/innovateria/firstapp/FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package in.innovateria.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+
+}
---
+++ firstapp/src/test/java/in/innovateria/firstapp/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.innovateria.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
